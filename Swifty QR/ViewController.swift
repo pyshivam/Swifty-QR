9bb3997691d24e46d5603858613abc8431a12fc1
pyshivam <pyshivam.py@gmail.com>
2018-11-09 00:10:23 +0530

    Initial Commit

---
+++ Swifty QR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty QR
+//
+//  Created by Shivam Gupta on 11/9/18.
+//  Copyright © 2018 PyShivam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
